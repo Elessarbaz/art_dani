1a96c54ea11f75783f76a3cb61a48de8ee4598b4
serg <aspirin_1988@mail.ru>
2016-07-21 11:13:33 +0600

    /start

--- wp-content/themes/art_dani/footer.php
+++ wp-content/themes/art_dani/footer.php
@@ -93,7 +93,18 @@
 <script src="<?php bloginfo('template_directory')?>/public/js/components/slider.min.js"></script>
 <script src="<?php bloginfo('template_directory')?>/public/js/components/slideshow.min.js"></script>
 <script src="<?php bloginfo('template_directory')?>/public/js/components/lightbox.min.js"></script>
-
+<script src="https://bsh.su/client/script/GET/"></script>
+<script>
+	var submitSMG = new BMModule();
+	submitSMG.submitForm(function(success) { $('.blink-mailer input[type=submit]').val('Отправить'); $('.blink-mailer input,.blink-mailer textarea').prop('disabled', true); $('.success-mail-text').html(success); $('.call_form-text').hide(500);  $('.success-mail-text').show(500);  }, function(error) {});
+</script>
+<script>
+	var el = document.querySelector('input[type="tel"]');
+	console.log();
+	VMasker(el).maskPattern("+9(999) 999-99-99"); // masking the input
+</script>
+<?=get_field('google',4)?>
+<?=get_field('yandex',4)?>
 <?php wp_footer() ?>
 </body>
 </html>
